f8be4d3bdd187c5c9c7d431b6b610ac0e586ed11
balraj1802 <codekarbalraj@gmail.com>
2025-09-20 06:20:16 +0000

    problem_solving

--- vector_database.py
+++ vector_database.py
@@ -2,15 +2,15 @@ from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
+import os
 
-#Step 1: Upload & Load raw PDF(s)
-
+# Step 1: Upload & Load raw PDF(s)
 pdfs_directory = 'pdfs/'
 
 def upload_pdf(file):
+    os.makedirs(pdfs_directory, exist_ok=True)
     with open(pdfs_directory + file.name, "wb") as f:
         f.write(file.getbuffer())
-
 
 def load_pdf(file_path):
     loader = PDFPlumberLoader(file_path)
@@ -20,26 +20,28 @@ def load_pdf(file_path):
 file_path = 'crop_recommendation.pdf'
 documents = load_pdf(file_path)
 
-#Step 2: Create Chunks
+# Step 2: Create Chunks
 def create_chunks(documents): 
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 200,
-        add_start_index = True
+        chunk_size=1000,
+        chunk_overlap=200,
+        add_start_index=True
     )
     text_chunks = text_splitter.split_documents(documents)
     return text_chunks
 
 text_chunks = create_chunks(documents)
-#print("Chunks count: ", len(text_chunks))
+print("✅ Chunks created:", len(text_chunks))
 
 #Step 3: Setup Embeddings Model (Use DeepSeek R1 with Ollama)
-ollama_model_name="deepseek-r1:14b"
+ollama_model_name="deepseek-r1:1.5b"
 def get_embedding_model(ollama_model_name):
     embeddings = OllamaEmbeddings(model=ollama_model_name)
     return embeddings
 
-#Step 4: Index Documents **Store embeddings in FAISS (vector store)
+# Step 4: Index Documents **Store embeddings in FAISS (vector store)
 FAISS_DB_PATH="vectorstore/db_faiss"
 faiss_db=FAISS.from_documents(text_chunks, get_embedding_model(ollama_model_name))
 faiss_db.save_local(FAISS_DB_PATH)
+
+print("✅ FAISS vector database created and saved at:", FAISS_DB_PATH)
